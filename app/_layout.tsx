da204ef24203a80ccbbabf357c9554094c8898ca
lenoben <lenoben002@gmail.com>
2024-06-27 19:19:03 +0300

    sign in

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,6 +28,10 @@ const RootLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      {/* hides a top bar with navigation links */}
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
     </Stack>
   );
 };
