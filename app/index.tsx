8ac6e9374fb50fdc05bade9e4f037714e9efbf9d
lenoben <lenoben002@gmail.com>
2024-06-27 18:41:33 +0300

    improved home screen design

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { ScrollView, Text, View, Image } from "react-native";
-import { Link } from "expo-router";
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "@/constants";
+import CustomButton from "@/components/CustomButton";
 
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -35,8 +36,16 @@ export default function App() {
             Where creativity meets innovation: embark on a journey of limitness
             expolration with Aora
           </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/signin")}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
+      {/*statusbar shows or hids things like battery percentage*/}
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 }
