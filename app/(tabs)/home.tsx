36f05f9aaefbbea2481a5c3a48034de5fefa0834
lenoben <lenoben002@gmail.com>
2024-06-30 23:10:14 +0300

    home tab and components

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,11 +1,73 @@
-import { View, Text } from "react-native";
-import React from "react";
+import EmptyState from "@/components/EmptyState";
+import SearchInput from "@/components/SearchInput";
+import Trending from "@/components/Trending";
+import { images } from "@/constants";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { View, Text, FlatList, Image, RefreshControl } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    // re call videos -> if any new videos appeared
+    console.log("refreshed");
+    setRefreshing(false);
+  };
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        // data={[{ $id: 1 }, { $id: 2 }, { $id: 3 }]}
+        data={[]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <Text className="text-3xl text-white">{item.$id}</Text>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  lenoben
+                </Text>
+              </View>
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMethod="contain"
+                />
+              </View>
+            </View>
+
+            <SearchInput />
+
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-gray-100 text-lg font-pregular mb-3">
+                Latest Videos
+              </Text>
+
+              <Trending posts={[{ $id: 1 }, { $id: 2 }, { $id: 3 }]} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No videos found"
+            subtitle="Be the first to upload a video"
+          />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 };
 
---
+++ components/SearchInput.tsx
@@ -0,0 +1,34 @@
+import { icons } from "@/constants";
+import { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+
+const SearchInput = ({
+  title,
+  value,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row space-x-4">
+      <TextInput
+        className="text-base mt-0.5 text-white flex-1 font-pregular"
+        value={value}
+        placeholder="Search for a video topic"
+        placeholderTextColor="#7b7b8b"
+        onChangeText={handleChangeText}
+        secureTextEntry={title === "Password" && !showPassword}
+      />
+      <TouchableOpacity>
+        <Image
+          source={icons.search}
+          className="w-9 h-10"
+          resizeMode="contain"
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchInput;
