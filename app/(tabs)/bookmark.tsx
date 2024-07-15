607df606e208f0f4d74ca8f41c201deb09786f97
lenoben <lenoben002@gmail.com>
2024-07-15 22:36:18 +0300

    bookmark

--- app/(tabs)/bookmark.tsx
+++ app/(tabs)/bookmark.tsx
@@ -1,11 +1,14 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Bookmark = () => {
   return (
-    <View>
-      <Text>Bookmark</Text>
-    </View>
+    <SafeAreaView className="px-4 my-6 bg-primary h-full">
+      <Text className="text-2xl text-white font-psemibold">Bookmark</Text>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 };
 
